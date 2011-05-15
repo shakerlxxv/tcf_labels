6ac034803cacd8086bc100d3e7db6a909b01c03c
Brian Shaver <brian@4-dogs.biz>
2011-05-15 19:41:05 -0400

    setup environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TcfLabels::Application.config.secret_token = '2899ecdca731cd8c7cab477e6d26b35751fa03caaf296e52229c687d78f5f5e05adeb59fe8c88e6e6ff7beab58980d62589594a161574cd14a9fff9d2e779b2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TcfLabels::Application.config.session_store :cookie_store, :key => '_tcf_labels_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TcfLabels::Application.config.session_store :active_record_store
